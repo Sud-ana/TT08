949c558c5a6ec414aa84ee6245c0169f5e501cdf
smishra <engr.sudhansu.mishra@gmail.com>
2024-09-06 10:26:40 +0000

    Placed ota5t_1 instance into analog template and then updated the project.v GL netlist for the instantiation.

--- src/project.v
+++ src/project.v
@@ -5,7 +5,7 @@
 
 `default_nettype none
 
-module tt_um_example (
+module tt_um_sudana_ota5t_1 (
     input  wire       VGND,
     input  wire       VDPWR,    // 1.8v power supply
 //    input  wire       VAPWR,    // 3.3v power supply
@@ -19,5 +19,47 @@ module tt_um_example (
     input  wire       clk,      // clock
     input  wire       rst_n     // reset_n - low to reset
 );
+
+//Instantiate the design using the name of the cell
+
+	ota5t_1 ota5t_1 (
+         .VDD(VDPWR),
+	 .VSS(VGND),
+	 .PLUS(ua[3]),
+	 .MINUS(ua[2]),
+	 .DIFFOUT(ua[0]),
+	 .BIAS(ua[1])
+	)
+//Tie all outputs low so that they aren't left floating 
+
+	assign uo_out[0] = VGND;
+	assign uo_out[1] = VGND;
+	assign uo_out[2] = VGND;
+	assign uo_out[3] = VGND;
+	assign uo_out[4] = VGND;
+	assign uo_out[5] = VGND;
+	assign uo_out[6] = VGND;
+	assign uo_out[7] = VGND;
+	//assign uo_out[8] = VGND;
+
+	assign uio_out[0] = VGND;
+	assign uio_out[1] = VGND;
+	assign uio_out[2] = VGND;
+	assign uio_out[3] = VGND;
+	assign uio_out[4] = VGND;
+	assign uio_out[5] = VGND;
+	assign uio_out[6] = VGND;
+	assign uio_out[7] = VGND;
+	//assign uio_out[8] = VGND;
+		
+	assign uio_oe[0] = VGND;
+	assign uio_oe[1] = VGND;
+	assign uio_oe[2] = VGND;
+	assign uio_oe[3] = VGND;
+	assign uio_oe[4] = VGND;
+	assign uio_oe[5] = VGND;
+	assign uio_oe[6] = VGND;
+	assign uio_oe[7] = VGND;
+	//assign uio_oe[8] = VGND;
 
 endmodule
